1d6d091b079bbefdb5f4e02a1162c27160461469
Jeff Knupp <jeff.knupp@enigma.io>
2016-01-27 16:30:30 -0500

    Try using buffered output to stdout

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net/http"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,18 +13,25 @@ import (
 
 var indexColor = color.New(color.FgYellow).SprintFunc()
 var occurenceColor = color.New(color.BgYellow, color.FgBlack).SprintFunc()
-var fileColor = color.New(color.FgGreen, color.Bold)
+var fileColor = color.New(color.FgGreen, color.Bold).SprintFunc()
 
 var target string
-var filesRead int
+var filesRead, binaryFiles, matches int
 
 func main() {
 	target = os.Args[1]
 	files := Walk(os.Args[2])
-	for _, file := range files {
-		inspectFile(file)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	for index, file := range files {
+		if index%1000 == 0 {
+			f.Flush()
+		}
+		inspectFile(file, f)
 	}
-	fmt.Printf("Read %d files", filesRead)
+	f.Write([]byte(fmt.Sprintf("Read %d files", filesRead)))
+	f.Write([]byte(fmt.Sprintf("Skipped %d binary files", binaryFiles)))
+	f.Write([]byte(fmt.Sprintf("Found %d matches", matches)))
 }
 
 func Walk(path string) []string {
@@ -47,38 +54,37 @@ func Walk(path string) []string {
 				continue
 			}
 			files = append(files, Walk(name)...)
+			continue
+		} else if !entry.Mode().IsRegular() {
+			continue
 		}
 		files = append(files, name)
 	}
 	return files
 }
 
-func inspectFile(path string) {
+func inspectFile(path string, f io.Writer) {
 	input, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("File open error: %v", err)
 		os.Exit(1)
 	}
-
 	occurrences := []string{}
 	scanner := bufio.NewScanner(input)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if http.DetectContentType([]byte(line)) == "application/octet-stream" {
-			return
-		}
 		if strings.Contains(line, target) {
+			matches++
 			occurrences = append(occurrences, line)
 		}
 	}
 	if len(occurrences) > 0 {
-		fmt.Println()
-		fileColor.Println(path)
-		fmt.Println()
+		f.Write([]byte("\n"))
+		f.Write([]byte(fmt.Sprintf("%s", fileColor(path))))
 		for index, i := range occurrences {
 			line := strings.Replace(i, target, occurenceColor(target), -1)
-			fmt.Printf("%s:%s\n", indexColor(index), line)
+			f.Write([]byte(fmt.Sprintf("%s:%s\n", indexColor(index), line)))
 		}
 	}
 }
